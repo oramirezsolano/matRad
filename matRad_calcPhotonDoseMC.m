35fb75ad3ab5e55265dffe376fc124a8dbbd20a3
oramirezsolano <oramirezsolano@gmail.com>
2019-03-18 18:01:22 -0300

    minor changes

--- matRad_calcPhotonDoseMC.m
+++ matRad_calcPhotonDoseMC.m
@@ -382,35 +382,6 @@ ompMCsource.zSide2 = bixelSide2(:,3);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 writeBeamletfileMCRes(file,ompMCsource.nBeams,ompMCsource.nBixels,beamSource,ompMCsource.iBeam,bixelCorner,bixelSide1,bixelSide2);
-% outputname = strtok(file,'.mat');
-% ext = '.beamlet';
-% 
-% fileID = fopen(strcat(outputname,ext),'w');
-% 
-% % Source position (x, y, z):
-% fprintf(fileID, '%d %d ', ompMCsource.nBeams, ompMCsource.nBixels);
-% fprintf(fileID, '\n');
-% for i = 1:ompMCsource.nBeams
-%     fprintf(fileID, '%f ', beamSource(i,1));
-%     fprintf(fileID, '%f ', beamSource(i,2));
-%     fprintf(fileID, '%f ', beamSource(i,3));  
-%     fprintf(fileID, '\n');
-% end
-% for i = 1:ompMCsource.nBixels    
-%     fprintf(fileID, '%d ', ompMCsource.iBeam(i)-1);  
-%     fprintf(fileID, '%f ', bixelCorner(i,1));
-%     fprintf(fileID, '%f ', bixelCorner(i,2));
-%     fprintf(fileID, '%f ', bixelCorner(i,3));
-%     fprintf(fileID, '%f ', bixelSide1(i,1));
-%     fprintf(fileID, '%f ', bixelSide1(i,2));
-%     fprintf(fileID, '%f ', bixelSide1(i,3));
-%     fprintf(fileID, '%f ', bixelSide2(i,1));
-%     fprintf(fileID, '%f ', bixelSide2(i,2));
-%     fprintf(fileID, '%f ', bixelSide2(i,3));
-%     fprintf(fileID, '\n');
-% end
-% 
-% fclose(fileID);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if visBool
